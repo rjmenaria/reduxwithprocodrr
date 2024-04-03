0534935b5e21789a1eabbe19275e94a392bc28e5
rjmenaria <ravimenaria847@gmail.com>
2024-04-03 12:21:36 +0530

    new

--- components/Header.jsx
+++ components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
       .catch((error) => dispatch(fetchProductError()));
 
     dispatch(fetchCartitems());
-    fetch("https://fakestoreapi.com/cabrts/5")
+    fetch("https://fakestoreapi.com/carts/5")
       .then((res) => res.json())
       .then((res) => dispatch(LoadCartitems(res)))
       .catch((error) => dispatch(fetchCartitemError(error)));
